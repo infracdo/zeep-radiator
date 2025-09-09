09b91d611458e15adae1ea754e41e8499e9dbe9d
infracdo <infracdo@gmail.com>
2025-09-09 10:36:38 +0800

    add missing . in session hook

--- session_hook.pl
+++ session_hook.pl
@@ -6,7 +6,7 @@ sub {
     if ($csid_clean) {
         # Add a new attribute 'Clean-Called-Station-Id' with the cleaned value
         $p->add_attr('Clean-Called-Station-Id', $csid_clean);
-        print $log scalar(localtime) . " - 📡 cleaned mac address " . $p->get_attr('Clean-Called-Station-Id')"\n";
+        print $log scalar(localtime) . " - 📡 cleaned mac address " . $p->get_attr('Clean-Called-Station-Id') . "\n";
     }
 
     open(my $log, '>>', '/var/log/radiator/session_debug.log');
