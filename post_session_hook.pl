0e8f4746aedff10e396f540d864bee9fbf1a5558
infracdo <infracdo@gmail.com>
2025-09-10 17:24:02 +0800

    fix global symbol error for dbh

--- post_session_hook.pl
+++ post_session_hook.pl
@@ -6,12 +6,12 @@ sub {
     my $username = $p->get_attr('User-Name');
     my $csid_raw = $p->get_attr('Called-Station-Id');
     my ($csid) = $csid_raw =~ /^([0-9a-fA-F]{12})/;
+    my ($dbh, $sth, $remaining_bytes);
 
     if ($result == $main::ACCEPT)
     {
-        my ($dbh, $sth, $remaining_bytes);
-        if ($code eq 'Access-Request') {
-            eval {
+        eval {
+            if ($code eq 'Access-Request') {
                 $dbh = DBI->connect("dbi:Pg:dbname=radius;host=192.168.61.22;port=5433", "radiator", "ap0ll0z33P", { RaiseError => 1, AutoCommit => 1 });
                 $sth = $dbh->prepare(q{
                     INSERT INTO nas_session_mac_attrs (username, called_station_id, updated_at)
@@ -26,22 +26,16 @@ sub {
                 open(my $log1, '>>', '/var/log/radiator/session_debug.log');
                 print $log1 scalar(localtime) . " - logged user $username and csid $csid to nas_session_mac_attrs table\n";
                 close($log1);
-            };
-            if ($@) {
-                open(my $errlog, '>>', '/var/log/radiator/session_debug.log');
-                print $errlog scalar(localtime) . " - POST-SESSION HOOK ENCOUNTERED DB ERROR 1: $@\n";
-                close($errlog);
-            }
-        } elsif ($code eq 'Accounting-Request') {
-            eval {
+            } elsif ($code eq 'Accounting-Request') {
+
                 $dbh = DBI->connect("dbi:Pg:dbname=radius;host=192.168.61.22;port=5433", "radiator", "ap0ll0z33P", { RaiseError => 1, AutoCommit => 1 });
-                $check_sth = $dbh->prepare(q{
+                $sth = $dbh->prepare(q{
                     SELECT remaining_bytes FROM subscriber WHERE username = ?
                 });
-                $check_sth->execute($username);
-                $remaining_bytes = $check_sth->fetchrow_array;
+                $sth->execute($username);
+                $remaining_bytes = $sth->fetchrow_array;
                 $remaining_bytes = 0 unless defined $remaining_bytes;
-                $check_sth->finish;
+                $sth->finish;
                 $dbh->disconnect;
 
                 open(my $log2, '>>', '/var/log/radiator/session_debug.log');
@@ -56,12 +50,12 @@ sub {
                     &main::log($main::LOG_DEBUG, "Access denied due to insufficient remaining bytes.");
                     ${$_[2]} = $main::REJECT;
                 }
-            };
-            if ($@) {
-                open(my $errlog, '>>', '/var/log/radiator/session_debug.log');
-                print $errlog scalar(localtime) . " - POST-SESSION HOOK ENCOUNTERED DB ERROR 2: $@\n";
-                close($errlog);
-            }
+            } 
+        };
+        if ($@) {
+            open(my $errlog, '>>', '/var/log/radiator/session_debug.log');
+            print $errlog scalar(localtime) . " - POST-SESSION HOOK ENCOUNTERED DB ERROR: $@\n";
+            close($errlog);
         }
     }
 
