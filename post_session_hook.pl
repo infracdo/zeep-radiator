1e5d89d8a37b45bf247086a14517a1a36cbc47f0
infracdo <infracdo@gmail.com>
2025-09-10 16:31:11 +0800

    add post auth hook for checking remaining bytes

--- post_session_hook.pl
+++ post_session_hook.pl
@@ -2,36 +2,62 @@ sub {
     my $p = ${$_[0]};
     my $rp = ${$_[1]};
     my $result = ${$_[2]};
-    
+    my $code   = $p->code;
     my $username = $p->get_attr('User-Name');
     my $csid_raw = $p->get_attr('Called-Station-Id');
     my ($csid) = $csid_raw =~ /^([0-9a-fA-F]{12})/;
 
     if ($result == $main::ACCEPT)
     {
-        
-    }
+        if ($code eq 'Access-Request') {
+            eval {
+                my $dbh = DBI->connect("dbi:Pg:dbname=radius;host=192.168.61.22;port=5433", "radiator", "ap0ll0z33P", { RaiseError => 1, AutoCommit => 1 });
+                my $sth = $dbh->prepare(q{
+                    INSERT INTO nas_session_mac_attrs (username, called_station_id, updated_at)
+                    VALUES (?, ?, NOW())
+                    ON CONFLICT (username)
+                    DO UPDATE SET called_station_id = EXCLUDED.called_station_id, updated_at = NOW()
+                });
+                $sth->execute($username, $csid);
+                $sth->finish;
+                $dbh->disconnect;
 
-    eval {
-        my $dbh = DBI->connect("dbi:Pg:dbname=radius;host=192.168.61.22;port=5433", "radiator", "ap0ll0z33P", { RaiseError => 1, AutoCommit => 1 });
-        my $sth = $dbh->prepare(q{
-            INSERT INTO nas_session_mac_attrs (username, called_station_id, updated_at)
-            VALUES (?, ?, NOW())
-            ON CONFLICT (username)
-            DO UPDATE SET called_station_id = EXCLUDED.called_station_id, updated_at = NOW()
-        });
-        $sth->execute($username, $csid);
-        $sth->finish;
-        $dbh->disconnect;
-
-        open(my $log2, '>>', '/var/log/radiator/session_debug.log');
-        print $log2 scalar(localtime) . " - Added $username and $csid to nas_session_mac_attrs\n";
-        close($log2);
-    };
-    if ($@) {
-        open(my $errlog, '>>', '/var/log/radiator/session_debug.log');
-        print $errlog scalar(localtime) . " - POST-SESSION HOOK ENCOUNTERED DB ERROR: $@\n";
-        close($errlog);
+                open(my $log2, '>>', '/var/log/radiator/session_debug.log');
+                print $log2 scalar(localtime) . " - logged user $username and csid $csid to nas_session_mac_attrs table\n";
+                close($log2);
+            };
+            if ($@) {
+                open(my $errlog, '>>', '/var/log/radiator/session_debug.log');
+                print $errlog scalar(localtime) . " - POST-SESSION HOOK ENCOUNTERED DB ERROR 1: $@\n";
+                close($errlog);
+            }
+        } else if ($code eq 'Accounting-Request') {
+            eval {
+                my $dbh = DBI->connect("dbi:Pg:dbname=radius;host=192.168.61.22;port=5433", "radiator", "ap0ll0z33P", { RaiseError => 1, AutoCommit => 1 });
+                my $check_sth = $dbh->prepare(q{
+                    SELECT remaining_bytes FROM subscriber WHERE username = ?
+                });
+                $check_sth->execute($username);
+                my $remaining_bytes = $check_sth->fetchrow_array;
+                $remaining_bytes = 0 unless defined $remaining_bytes;
+                $check_sth->finish;
+                $dbh->disconnect;
+                
+                if ($remaining_bytes <= 0)
+                {
+                    open(my $rejlog, '>>', '/var/log/radiator/session_debug.log');
+                    print $rejlog scalar(localtime) . " - user has no remaining bytes left - rejecting $username\n";
+                    close($rejlog);
+                    &main::log($main::LOG_DEBUG, "Access denied due to insufficient remaining bytes.");
+                    ${$_[2]} = $main::REJECT;
+                }
+            };
+            if ($@) {
+                open(my $errlog, '>>', '/var/log/radiator/session_debug.log');
+                print $errlog scalar(localtime) . " - POST-SESSION HOOK ENCOUNTERED DB ERROR 2: $@\n";
+                close($errlog);
+            }
+        }
     }
 
     return;
