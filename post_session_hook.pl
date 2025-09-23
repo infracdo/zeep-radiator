b01fdf2483b4a4e08e1fadc36981295b25bb94c3
infracdo <infracdo@gmail.com>
2025-09-23 09:56:24 +0800

    Update post_session_hook.pl

--- post_session_hook.pl
+++ post_session_hook.pl
@@ -13,7 +13,7 @@ sub {
     {
         if ($username && $csid) {
             eval {
-                $dbh = DBI->connect("dbi:Pg:dbname=radius;host=192.168.61.22;port=5433", "radiator", "ap0ll0z33P", { RaiseError => 1, AutoCommit => 1 });
+                $dbh = DBI->connect("dbi:Pg:dbname=radius;host=192.168.61.37;port=5433", "radiator", "z33PRad!aT0r", { RaiseError => 1, AutoCommit => 1 });
                 $sth = $dbh->prepare(q{
                     INSERT INTO nas_session_mac_attrs (username, called_station_id, updated_at)
                     VALUES (?, ?, NOW())
